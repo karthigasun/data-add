cc5af80576e99efa55f177f601d0c94400610142
karthigasun <prkarthik3@gmail.com>
2022-12-05 18:28:04 +0530

    up

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 function data() {
-    var numEle = document.getElementById("no").value,
-  nameEle = document.getElementById("name").value,
-  ageEle = document.getElementById("age").value,
-  paraEle = document.getElementById("para");
-
-  if (numEle == "" || nameEle == "" || ageEle == "") {
-    paraEle.innerHTML = "Please Enter The Details Correctly";
+  var numEle = document.getElementById("no").value,
+    nameEle = document.getElementById("name").value,
+    ageEle = document.getElementById("age").value,
+    paraEle = document.getElementById("para");
+  nameEle = nameEle.trim();
+  nameEle = nameEle.split(" ").join("");
+  if (numEle == "") {
+    paraEle.innerHTML = "Please Enter The All Details";
   } 
   else {
-    		var html="";
+    var html = "";
 
-		html="<tr><td>"+numEle+"</td><td>"+nameEle+"</td><td>"+ageEle+"</td></tr>";
+    html =
+      "<tr><td>" +
+      numEle +
+      "</td><td>" +
+      nameEle +
+      "</td><td>" +
+      ageEle +
+      "</td></tr>";
 
-		document.getElementById('bind').innerHTML+=html;
+    document.getElementById("bind").innerHTML += html;
 
-		document.getElementById('no').value="";
-		document.getElementById('name').value="";
-		document.getElementById('age').value="";
+    document.getElementById("no").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
   }
 }
